ac1139ee8c81a39bd92f8be58c89fdcf1475876e
胡超 <huchao@zafh.com.cn>
2017-09-22 18:29:43 +0800

    test

--- app/src/main/java/com/cheguo/camerasdkdemo/MainActivity.java
+++ app/src/main/java/com/cheguo/camerasdkdemo/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+                intent.putExtra("test","test");
                 startActivity(intent);
             }
         });
